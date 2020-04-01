bb83601cc81c14a545a3be849903379b16db9355
TheDeadFish <michaelstamper1@gmail.com>
2020-04-01 07:07:13 +0000

    1. added basic test: tests/hello.cc

---
+++ tests/hello.cc
@@ -0,0 +1,8 @@
+#include "stdshit.h"
+const char progName[] = "hello";
+
+int main(int argc, char* argv[])
+{
+	xstr str = xstrfmt("%d, %X, %f, %z\n", 10, 10, 10.3, "hello you fat shit");
+	contError(NULL, "hello: %s, %s", argv[0], str.data);
+}
