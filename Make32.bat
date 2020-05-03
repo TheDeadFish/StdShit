12c4be6501ec5ec67e7ca20447d4bcec9e7e5aa6
TheDeadFish <michaelstamper1@gmail.com>
2020-05-03 19:34:08 +0000

    MakeXX.bat: use gmake_gcc batch file to build

--- Make32.bat
+++ Make32.bat
@@ -1,14 +1,8 @@
-@setlocal
-@call egcc.bat
-@if not exist build32 mkdir build32
-@pushd build32
-@set CFLAGS=%CFLAGS2%
-@set CXXFLAGS=%CCFLAGS2%
-@%CMAKE% ..
-@mingw32-make
-@popd
+@call cmake_gcc
 
 rem install stdshit
+@setlocal
+@call egcc.bat
 copy /Y build32\CMakeFiles\stdshit.dir\cmake_pch.hxx.gch %prefix%\include\stdshit.h.gch
 copy /Y bin\stdshit.h %prefix%\include
 copy /Y bin\stdshit.h %PROGRAMS%\local\include
--- Make64.bat
+++ Make64.bat
@@ -1,14 +1,8 @@
-@setlocal
-@call egcc64.bat
-@if not exist build64 mkdir build64
-@pushd build64
-@set CFLAGS=%CFLAGS2%
-@set CXXFLAGS=%CCFLAGS2%
-@%CMAKE% ..
-@mingw32-make
-@popd
+@call cmake_gcc x64
 
 rem install stdshit
+@setlocal
+@call egcc.bat x64
 copy /Y build64\CMakeFiles\stdshit.dir\cmake_pch.hxx.gch %prefix%\include\stdshit.h.gch
 copy /Y bin\stdshit.h %prefix%\include
 copy /Y bin\stdshit.h %PROGRAMS%\local\include
